b662d7b29448697de6e6f01ddbaba650959ef1c2
khaelar <midavmidav2000@gmail.com>
2022-05-26 21:31:23 +0300

    revert: daemon starting using shell

--- ctl.py
+++ ctl.py
@@ -24,7 +24,7 @@ def entry(action, args={}, body_raw=b""):
 def start():
 	pathy_dir = Path(__file__).parent
 	daemon_file = pathy_dir / "daemon.py"
-	subprocess.Popen(f"python3 {str(daemon_file)} start", shell=True)
+	subprocess.Popen(["python3", daemon_file, "start"])
 
 def send(msg, args={}):
 	conn = Client(DAEMON_ADDR, authkey=DAEMON_AUTHKEY)
