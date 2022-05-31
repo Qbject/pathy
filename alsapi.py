889d26a9ba0e5c575b3754098303474b8551c7cf
khaelar <midavmidav2000@gmail.com>
2022-05-31 19:21:12 +0300

    feat: added more ALS API interaction methods

--- alsapi.py
+++ alsapi.py
@@ -4,8 +4,8 @@ from const import *
 
 def get_player_stat(player_uid):
 	url = f"https://api.mozambiquehe.re/bridge?version=5&platform=PC" \
-	f"&uid={player_uid}&auth={MOZAM_API_KEY}&merge=true&removeMerged=true"
-	resp = requests.get(url)
+	f"&uid={player_uid}&merge=true&removeMerged=true"
+	resp = requests.get(url, headers={"Authorization": MOZAM_API_KEY})
 	
 	if resp.status_code == 429:
 		time.sleep(1)
@@ -26,7 +26,44 @@ def get_player_stat(player_uid):
 	return stat
 
 def get_map_rotation():
-	pass # TODO
+	url = f"https://api.mozambiquehe.re/maprotation?version=5"
+	resp = requests.get(url, headers={"Authorization": MOZAM_API_KEY})
+	
+	if resp.status_code == 429:
+		time.sleep(1)
+		return get_map_rotation()
+	
+	if not resp.ok:
+		raise AlsApiError(f"ALS API respond with code " \
+			f"{resp.status_code}: {resp.reason}")
+	
+	try:
+		rotation = resp.json()
+	except Exception as e:
+		log(f"ALS API invalid map rotation response:\n{resp.text}")
+		raise AlsApiError(f"ALS API respond with invalid data")
+	
+	return rotation
+
+def get_craft_rotation():
+	url = f"https://api.mozambiquehe.re/crafting"
+	resp = requests.get(url, headers={"Authorization": MOZAM_API_KEY})
+	
+	if resp.status_code == 429:
+		time.sleep(1)
+		return get_craft_rotation()
+	
+	if not resp.ok:
+		raise AlsApiError(f"ALS API respond with code " \
+			f"{resp.status_code}: {resp.reason}")
+	
+	try:
+		rotation = resp.json()
+	except Exception as e:
+		log(f"ALS API invalid craft rotation response:\n{resp.text}")
+		raise AlsApiError(f"ALS API respond with invalid data")
+	
+	return rotation
 
 class AlsApiError(Exception):
 	pass
