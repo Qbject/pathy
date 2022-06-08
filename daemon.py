f683807eb0d34ec50b2cdcae3faeb42c0e97c62f
khaelar <midavmidav2000@gmail.com>
2022-06-08 12:03:11 +0300

    fix: wrong variable names

--- daemon.py
+++ daemon.py
@@ -89,8 +89,8 @@ class PathyDaemon():
 		elif msg == "setdelay":
 			self.state["player_fetch_delay"] = int(args.get("delay", 1))
 		elif msg == "tgupd":
-			self.handle_tg_upd(args.get("tg_upd"))
-			# self.as_worker(self.handle_tg_upd, args.get("tg_upd"))
+			self.handle_tg_upd(args.get("upd_body"))
+			# self.as_worker(self.handle_tg_upd, args.get("upd_body"))
 			return "DONE"
 		else:
 			return "UNKNOWN_MSG"
