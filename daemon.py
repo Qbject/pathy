713f50035153d0976a28c928019ee1b3504cc947
khaelar <midavmidav2000@gmail.com>
2022-06-27 20:25:49 +0300

    fix: daemon syntax error

--- daemon.py
+++ daemon.py
@@ -126,7 +126,7 @@ class PathyDaemon():
 				if upd_resp["went_online"]:
 					self.handle_party_events(player)
 		except Exception as e:
-			log(f"Player '{player.name}' update error, throttling\n"
+			log(f"Player '{player.name}' update error, throttling\n" +
 				traceback.format_exc(), err=True, send_tg=True)
 			time.sleep(10)
 		finally:
