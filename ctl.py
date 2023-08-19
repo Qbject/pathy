7acc73b6f99a2c1dd0f20a7a96cd368cc8b88c4c
khaelar <midavmidav2000@gmail.com>
2023-08-19 16:50:52 +0300

    refactored run_id handling

--- ctl.py
+++ ctl.py
@@ -38,9 +38,6 @@ def entry(action, args={}, body_raw=b"", from_web=False):
 		elif action == "tgupd":
 			return handle_tg_upd(body_raw)
 		
-		elif action == "run_id":
-			return send("run_id")
-		
 		else:
 			ensure_running()
 			resp = send(action, **args)
